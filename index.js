f4a3cd12274de859fff927790d6bac6cf97fad42
pieter19998 <pieter1998@protonmail.com>
2020-04-07 19:42:32 +0200

    fixed port for heroku

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('./src/app');
 const config = require('./config/config');
 
-app.listen(config.local.port, () => {
-    console.log(`Running on port ${config.local.port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`app is running on port ${ PORT }`);
 });
